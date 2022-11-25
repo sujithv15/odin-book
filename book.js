8eec00cdf968a51a82acf1000afbfa23ab17702a
sujithv15 <64935098+sujithv15@users.noreply.github.com>
2022-11-24 23:14:30 -0500

    logic and html fully working- need to style

--- book.js
+++ book.js
@@ -20,20 +20,25 @@ function addBook() {
     const author = document.getElementById('author-form').value;
     const numPages = document.getElementById('numPages-form').value;
     const isRead = document.querySelector('input[name = read]:checked').value;
-
+    const alertEl = document.getElementById('user-alert');
+    alertEl.textContent = '';
         // validate user entered all inputs, or else exit addBook() and add listener again
     if (!bookTitle || !author || !numPages || !isRead) {
         console.log('input error');
+            // message is set to be cleared above, so it does not stay there
+        alertEl.textContent = 'Please fill out all info!';
         loadSubmitListener();
     }
+    else {
         // create book object from variables that user entered in form
-    const newBook = new Book(bookTitle, author, numPages, isRead);
+        const newBook = new Book(bookTitle, author, numPages, isRead);
         // push book to library
-    myLibrary.push(newBook);
+        myLibrary.push(newBook);
         // add new book to library array, which will then shown on the display
-    addBookToDisplay(newBook);
+        addBookToDisplay(newBook);
         // once again load listener in case user wants to add another book
-    loadSubmitListener();
+        loadSubmitListener();
+    }
 }
 
     // Add book to the DOM and display to user
